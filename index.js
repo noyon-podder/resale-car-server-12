cdf77eca9adda8a1e07995cca7fbba7cff7f9cfa
noyon-podder <noyon.vai369@gmail.com>
2022-11-25 02:56:43 +0600

    get all category

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 
@@ -11,6 +12,28 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tlsofwm.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    const categoryCollection = client.db('resaleCarDB').collection('category');
+    const usersCollection = client.db('resaleCarDB').collection('users');
+
+
+    app.get('/category', async(req, res) => {
+        const query = {}
+        const result = await categoryCollection.find().toArray();
+        res.send(result)
+    })
+    app.post('/users', async(req, res) => {
+        const user = req.body;
+        const cursor = await usersCollection.insertOne(user);
+        res.send(cursor);
+    })
+}
+run().catch(console.log)
 
 app.get('/', async(req, res) => {
     res.send('resale server was running')
