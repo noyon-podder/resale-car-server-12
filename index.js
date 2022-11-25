56a85751d387c27de08a0b2a4ff70204a6583c36
noyon-podder <noyon.vai369@gmail.com>
2022-11-25 15:26:29 +0600

    booking data server send

--- index.js
+++ index.js
@@ -19,19 +19,40 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
     const categoryCollection = client.db('resaleCarDB').collection('category');
+    const carsCollection = client.db('resaleCarDB').collection('cars');
     const usersCollection = client.db('resaleCarDB').collection('users');
-
-
+    const bookingsCollection = client.db('resaleCarDB').collection('bookings');
+   
+    //category collection here
     app.get('/category', async(req, res) => {
         const query = {}
-        const result = await categoryCollection.find().toArray();
+        const result = await categoryCollection.find(query).toArray();
         res.send(result)
+    })
+
+    app.get('/category/:categoryId', async(req, res) => {
+        const category = req.params.categoryId;
+        const query = {
+            categoryId: category
+        }
+        const cars = await carsCollection.find(query).toArray();
+        res.send(cars)
     })
     app.post('/users', async(req, res) => {
         const user = req.body;
         const cursor = await usersCollection.insertOne(user);
         res.send(cursor);
     })
+
+    
+    //bookings collection 
+
+    app.post('/bookings', async(req, res) => {
+        const booking = req.body
+        const cursor = await bookingsCollection.insertOne(booking);
+        res.send(cursor);
+    })
+    
 }
 run().catch(console.log)
 
