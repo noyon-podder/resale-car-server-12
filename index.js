533866805c8242bcd5b8c381e42d15d6b346d6e2
noyon-podder <noyon.vai369@gmail.com>
2022-11-30 01:59:40 +0600

    required info deleted

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
