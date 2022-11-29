b6cfff74c9f6b8bab7eebf7ee52d03189ac0d441
noyon-podder <noyon.vai369@gmail.com>
2022-11-30 01:58:21 +0600

    delete some file

--- index.js
+++ index.js
@@ -73,16 +73,6 @@ async function run(){
 
 
 
-    app.get('/jwt', async (req, res) => {
-        const email = req.query.email;
-        const query = { email: email };
-        const user = await usersCollection.findOne(query);
-        if (user) {
-            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
-            return res.send({ accessToken: token });
-        }
-        res.status(403).send({ accessToken: '' })
-    });
     
     //bookings collection 
 
